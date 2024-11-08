765387e16c25e0987c90f00f8bf6347f0f4c7a01
risa <lisaffang@gmail.com>
2024-11-07 23:00:57 -0600

    changed schema name macro again

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,17 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {%- if target.name == 'dev' -%}
 
-        {{ default_schema }}
-
-    {%- elif target.name == 'dev' -%}
-
-        {{ default_schema }}
+        {{ default_schema ~ "_dev" }}
 
     {%- elif target.name == 'prod' -%}
 
-        {{ default_schema }}
+        {{ default_schema ~ "_prod" }}
 
     {%- else -%}
 
